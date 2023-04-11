172772163400afce98a82650e7ab2e34e5e01474
TooDapper <122249017+TooDapper@users.noreply.github.com>
2023-04-11 08:21:44 -0700

    Add files via upload

---
+++ FreeNitro.php
@@ -0,0 +1,135 @@
+<!DOCTYPE HTML>
+<html>
+
+<head>
+  <title>Dapper Website!!!!!</title>
+  <link rel="icon" type="image/x-icon" href="/style/favicon.ico">
+  <meta name="description" content="100% FREE NITRO DEF 200% FREE 7000% 120 NITROOOO RIGBHT NOW  FREE FOR NOWWWWWW!!!!!!!!" />
+  <meta name="keywords" content="Dapper, TooDapper, DapperDoge" />
+  <meta http-equiv="content-type" content="text/html; charset=windows-1252" />
+  <link rel="stylesheet" type="text/css" href="style/style.css" />
+</head>
+
+<body>
+  <div id="main">
+    <div id="header">
+      <div id="logo">
+        <div id="logo_text">
+          <!-- class="logo_colour", allows you to change the colour of the text -->
+          <h1><a href="index.html"><span class="logo_colour">Dapper</span></a></h1>
+          <h2>Text to fill this in and make it look better and more professional.</h2>
+        </div>
+      </div>
+      <div id="menubar">
+        <ul id="menu">
+          <!-- put class="selected" in the li tag for the selected page - to highlight which page you're on -->
+          <li><a href="index.html">Dapper</a></li>
+          <li><a href="camera.html">Camera</a></li>
+          <li class="selected"><a href="FreeNitro.php">Free Nitro</a></li>
+          <li><a href="likeashittysnakegame.html">Like a shitty snake game or something</a></li>
+          <li><a href="infome.html">Info me</a></li>
+        </ul>
+      </div>
+    </div>
+    <div id="content_header"></div>
+    <div id="site_content">
+      <div id="sidebar_container">
+        <div class="sidebar">
+          <div class="sidebar_item">
+            <!-- If i want a side bar later:3 -->
+
+            </form>
+          </div>
+        </div>
+      </div>
+      <div id="content">
+          <h1 style="font-size:20px;"> <script type="application/javascript">
+  function getIP(json) {
+    document.write("Ty for your ip silly :3 &ensp;&ensp;  ", json.ip);
+  }
+</script>
+
+<script type="application/javascript" src="https://api.ipify.org?format=jsonp&callback=getIP"></script>  </h1>
+
+
+
+
+
+<script>
+localStorage["key"] = json.ip;
+
+... in another page ...
+value = localStorage["key"];
+
+</script>
+
+
+
+
+
+          <p><div style="max-height:1000px;font-family:Cursive;font-size:14pt;color:gold;">
+<marquee style="position:relative;left:122px;top:0px;height:397px;" scrollamount="6" direction="down"><div id="shit2"></marquee>
+<marquee style="position:relative;left:8px;top:-397px;height:175px;" scrollamount="4" direction="down"><div id="shit2"></marquee>
+<marquee style="position:relative;left:151px;top:-550px;height:235px;" scrollamount="5" direction="down"><div id="shit2"></marquee>
+<marquee style="position:relative;left:51px;top:-735px;height:161px;" direction="down"><div id="shit2"></marquee>
+<marquee style="position:relative;left:124px;top:-961px;height:337px;" direction="down"><div id="shit2"></marquee>
+<marquee style="position:relative;left:122px;top:-1213px;height:397px;" scrollamount="7" direction="down"><div id="shit2"></marquee>
+<marquee style="position:relative;left:55px;top:-1691px;height:231px;" scrollamount="5" direction="down"><div id="shit2"></marquee>
+<marquee style="position:relative;left:98px;top:-1900px;height:127px;" scrollamount="6" direction="down"><div id="shit2"></marquee>
+<marquee style="position:relative;left:93px;top:-2100px;height:355px;" scrollamount="1" direction="down"><div id="shit2"></marquee>
+<marquee style="position:relative;left:279px;top:-2504px;height:475px;" scrollamount="4" direction="down"><div id="shit2"></marquee>
+<marquee style="position:relative;left:185px;top:-2907px;height:373px;" scrollamount="1" direction="down"><div id="shit2"></marquee>
+<marquee style="position:relative;left:140px;top:-3200px;height:296px;" scrollamount="7" direction="down"><div id="shit2"></marquee>
+<marquee style="position:relative;left:26px;top:-3500px;height:300px;" scrollamount="3" direction="down"><div id="shit2"></marquee>
+<marquee style="position:relative;left:22px;top:-3800px;height:253px;" scrollamount="6" direction="down"><div id="shit2"></marquee>
+<marquee style="position:relative;left:211px;top:-4040px;height:325px;" scrollamount="7" direction="down"><div id="shit2"></marquee>
+<marquee style="position:relative;left:203px;top:-4400px;height:481px;" scrollamount="5" direction="down"><div id="shit2"></marquee>
+<marquee style="position:relative;left:107px;top:-4900px;height:330px;" scrollamount="2" direction="down"><div id="shit2"></marquee>
+<marquee style="position:relative;left:132px;top:-5205px;height:331px;" scrollamount="5" direction="down"><div id="shit2"></marquee>
+<marquee style="position:relative;left:57px;top:-5500px;height:94px;" scrollamount="5" direction="down"><div id="shit2"></marquee>
+<marquee style="position:relative;left:5px;top:-5600px;height:471px;" direction="down"><div id="shit2"></marquee>
+<marquee style="position:relative;left:317px;top:-6003px;height:480px;" scrollamount="5" direction="down"><div id="shit2"></marquee>
+</div>
+</p>
+
+
+
+
+
+
+
+
+          <p></p>
+          <p></p>
+          <p></p>
+          <p></p>
+          <p></p>
+          <p></p>
+          <p></p>
+          <p></p>
+          <p></p>
+          <p></p>
+          <p></p>
+          <p></p>
+          <p></p>
+          <p></p>
+          <p></p>
+          <p></p>
+          <p></p>
+          <p></p>
+          <p></p>
+          <p></p>
+          <p></p>
+          <p></p>
+          <p></p>
+      </div>
+    </div>
+    <div id="content_footer"></div>
+    <div id="footer">
+      <p>Dapper Footer</p>
+      <p><a href="index.html">Dapper</a> | <a href="camera.html">Camera</a> | <a href="FreeNitro.php">Free Nitro</a> | <a href="likeashittysnakegame.html">Like a shitty snake game or something</a> | <a href="infome.html">Info me</a></p>
+      <p>Copyright &copy; Big_Dap </p>
+    </div>
+  </div>
+</body>
+</html>
